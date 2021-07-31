0670718561722734c2a1ab994b4e5a5375d7f219
Robson Lima <rsl.robson@gmail.com>
2021-07-31 17:10:57 -0300

    Using view binding in the MainActivity. Adding listener to FAB in order to invoke AddTaskActivity.

--- app/src/main/java/br/com/dio/todolist/MainActivity.kt
+++ app/src/main/java/br/com/dio/todolist/MainActivity.kt
@@ -1,11 +1,27 @@
 package br.com.dio.todolist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.dio.todolist.databinding.ActivityMainBinding
+import br.com.dio.todolist.ui.AddTaskActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        insertListeners()
+    }
+
+    private fun insertListeners() {
+        binding.fab.setOnClickListener {
+            // Adding listener on FAB to start 'AddTaskActivity'
+            startActivity(Intent(this, AddTaskActivity::class.java))
+        }
     }
 }
